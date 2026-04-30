7045fb751a081300c65ef11f9bd8cf6e488bc25e
ydh <sayurrdahh@naver.com>
2026-04-30 12:55:54 +0900

    commit

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -117,6 +117,36 @@ public class BasicController {
         return "basic/each";
     }
 
+    //조건부 평가
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    //주석
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    //블록
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    //자바스크립트 인라인
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user",new User("userA",10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
         list.add(new User("userA",10));
@@ -125,4 +155,5 @@ public class BasicController {
 
         model.addAttribute("users",list);
     }
+
 }
